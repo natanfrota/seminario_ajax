8ca6ab939bbbc658392493d23b69f22734b9fd0e
natanfrota <natanfrota31@gmail.com>
2025-09-16 00:23:04 -0300

    fix: comenta url

--- usuario/urls.py
+++ usuario/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.exibir_usuarios, name='exibir_usuarios'),
+    #path('', views.exibir_usuarios, name='exibir_usuarios'),
     path('api/usuarios', views.usuario_busca, name='usuario_busca'),
 ]
